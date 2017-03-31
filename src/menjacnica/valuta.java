a78ab0932b0cd7b090d4074b77dad36bdbc2bee5
NikolaVeselinovic <Dzoni@Dzoni-PC>
2017-03-31 22:37:10 +0200

    Dodate metode hashCode, equals i toString u klasi Valuta.

--- src/menjacnica/valuta.java
+++ src/menjacnica/valuta.java
@@ -38,5 +38,52 @@ public class valuta {
 	public void setKupovniKurs(double kupovniKurs) {
 		this.kupovniKurs = kupovniKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		valuta other = (valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "valuta [naziv=" + naziv + ", datum=" + datum + ", prodajniKurs=" + prodajniKurs + ", srednjiKurs="
+				+ srednjiKurs + ", kupovniKurs=" + kupovniKurs + "]";
+	}
 	
 }
